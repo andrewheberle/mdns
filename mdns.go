7c1f5f91417678b5ef49683504e4bcb64f152c1c
Andrew Heberle <andrew.heberle@gmail.com>
2022-08-31 21:08:35 +0800

    Add some vars

--- mdns.go
+++ mdns.go
@@ -1,5 +1,34 @@
 package mdns
 
+import "net"
+
+var (
+	// Multicast groups used by mDNS
+	GroupIPv4 = net.IPv4(224, 0, 0, 251)
+	GroupIPv6 = net.ParseIP("ff02::fb")
+
+	// mDNS wildcard addresses
+	WildcardAddrIPv4 = &net.UDPAddr{
+		IP:   net.ParseIP("224.0.0.0"),
+		Port: 5353,
+	}
+	WildcardAddrIPv6 = &net.UDPAddr{
+		IP: net.ParseIP("ff02::"),
+		// IP:   net.ParseIP("fd00::12d3:26e7:48db:e7d"),
+		Port: 5353,
+	}
+
+	// mDNS endpoint addresses
+	Ipv4Addr = &net.UDPAddr{
+		IP:   GroupIPv4,
+		Port: 5353,
+	}
+	Ipv6Addr = &net.UDPAddr{
+		IP:   GroupIPv6,
+		Port: 5353,
+	}
+)
+
 // PackClass packs a query or RR class setting either the unicast-response or cache-flush
 // bit respectively
 func PackClass(class uint16, flag bool) uint16 {
